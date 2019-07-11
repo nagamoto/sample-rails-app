b42fb910bdd140d09fe6cbb25ea42afd8b36a2bf
nagamoto <ngmt83ngmt@gmail.com>
2019-07-11 16:19:36 +0900

    外部キー制約をusersに変更

--- db/migrate/20190711071444_create_direct_messages.rb
+++ db/migrate/20190711071444_create_direct_messages.rb
@@ -1,8 +1,8 @@
 class CreateDirectMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :direct_messages do |t|
-      t.references :sender, foreign_key: true
-      t.references :reciever, foreign_key: true
+      t.references :sender, foreign_key: { to_table: :users }
+      t.references :reciever, foreign_key: { to_table: :users }
       t.text :body
 
       t.timestamps
